c1219d2fa878771da6c962b826688a8b90c2d784
fion-bao <cbao9@fordham.edu>
2020-02-19 00:21:45 -0500

    HelloWorld

---
+++ Self-learning/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld
+{
+    
+    private static void main(String args[]) {
+        System.out.println("Hello World");
+	System.out.println("1");
+	System.out.println("Hello World");
+	System.out.println("2");
+    }
+   
+}
